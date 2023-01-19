d6c6b3fccb31578422eb45029908ac324ea58aa0
Entropy675 <sibteali@outlook.com>
2023-01-19 18:54:45 -0500

    added main game loop, toggle size

--- Pos.h
+++ Pos.h
@@ -4,19 +4,19 @@
 class Pos
 {
 	public:
-	
+
 	Pos(int x = 0, int y = 0);
 
-	
+
 	void set(int ix, int iy);
 	void setX(int ix);
 	void setY(int iy);
-	
+
 	int getX();
 	int getY();
 	
-  bool operator==(const Pos &p);
-  bool operator!=(const Pos &p);
+	bool operator==(const Pos &p);
+	bool operator!=(const Pos &p);
     
 	private:
 	bool isValid(int ix, int iy); // check if the position is valid, for internal use, do not save invalid pos
--- main.cc
+++ main.cc
@@ -1,7 +1,13 @@
 #include <ncurses.h>
 #include <locale.h>
 #include <wchar.h>
+#include <string>
+#include <cstring>
 #include "defs.h"
+#include "Pos.h"
+#include "Board.h"
+
+using namespace std;
 
 int main() 
 {
@@ -12,93 +18,60 @@ int main()
 
 	attron(A_BOLD);
 
-	// lets get the size of a square on the screen
-	int sqX = 6; // accounting for border
-	int sqY = 3;
-
-
-	// example setup board
-	cchar_t pawn;
-	wchar_t pwide = L'♟';
-	setcchar(&pawn, &pwide, A_NORMAL, 0, NULL);
 	
-	drawBoard(sqX, sqY);
-	//mvprintw(7, 1, "Screen size: %dx%d sqaresize: %dx%d", COLS, LINES, sqX, sqY);
+	Board* game = new Board();
 
 
-	for(int x = 0; x < MAX_ROW_COL; x++)
+	game->drawBoard();
+	mvprintw(25, 1, "Board size: %dx%d sqaresize: %dx%d", game->sqSize.getY()*MAX_ROW_COL, game->sqSize.getX()*MAX_ROW_COL, 
+				game->sqSize.getX(), game->sqSize.getY());
+	
+
+	
+	while(true)
 	{
-		for(int y = 0; y < MAX_ROW_COL; y++)
+		
+		move(game->sqSize.getY()*MAX_ROW_COL + 1, 0);
+		string uinp; // overflow issues lmao
+		char ch;
+		while(true)
 		{
-			move(x*sqY + 1, y*sqX + 3);
-			add_wch(&pawn);
+			ch = getch();
+			addch(ch);
+			uinp += ch;
+			refresh();
+			if(ch == '\n')
+			{
+				break;
+			}
 		}
+		
+		uinp.erase(uinp.end()-1); // null terminates, removes \n from uinp
+		//addstr(uinp.c_str());
+		
+		if(uinp == string("tg"))
+		{
+			game->toggleSize();
+			refresh();
+		}
+		else if(uinp == string("exit"))
+			break;
+			
+		clear();
+		game->drawBoard();	
 	}
+	
+	
 	move(0, 0);
-	/*
-	move(1, 1);
-
-	for(int i = 0; i < MAX_ROW_COL; i++)
-		add_wch(&pawn); // Add the pawn character to the current cursor position
-		
-	move(2, 1);
-
-	for(int i = 0; i < MAX_ROW_COL; i++)
-		add_wch(&pawn); 
-
-	move(5, 1);
-
-	for(int i = 0; i < MAX_ROW_COL; i++)
-		add_wch(&pawn);
-
-	move(6, 1);
-
-	for(int i = 0; i < MAX_ROW_COL; i++)
-		add_wch(&pawn);
-	*/
-		
 
 	refresh();
-	getch();
+	//getch();
 	endwin();
 	return 0;
 }
 
-void drawBoard(int sqX, int sqY)
-{
-	cchar_t li;
-	wchar_t liwide = L'─';
-	setcchar(&li, &liwide, A_NORMAL, 0, NULL);
 
-	cchar_t ld;
-	wchar_t ldwide = L'│';
-	setcchar(&ld, &ldwide, A_NORMAL, 0, NULL);
 
-	for(int x = 0; x <= MAX_ROW_COL; x++)
-	{
-		for(int line = 1; line < sqY*MAX_ROW_COL; line++)
-		{
-			move(line, sqX*x);
-			add_wch(&ld);
-		}
-	}
 
-	for(int y = 0; y <= MAX_ROW_COL; y++)
-	{
-		move(sqY*y, 1);
-		for(int line = 0; line < sqX*MAX_ROW_COL - 1; line++)
-			add_wch(&li);
-		//move(sqX*x + sqX/2 + 1, sqY*y + sqY/2 + 1);
-		//add_wch(&pawn);	
-		
-	}
 
-	for(int x = 1; x <= MAX_ROW_COL -1; x++)
-	{
-		for(int y = 1; y <= MAX_ROW_COL -1; y++)
-		{
-			move(sqY*y, sqX*x);
-			addstr("+");
-		}
-	}
-}
+

c715441d4469596cd4366db0b7b69f21dda84d55
Entropy675 <sibteali@outlook.com>
2023-02-07 15:58:11 -0500

    Refactored ncurses code out of game code

--- Board.h
+++ Board.h
@@ -6,13 +6,6 @@
 #include <vector>
 #include <string>
 
-#include "pieces/Pawn.h"
-#include "pieces/King.h"
-#include "pieces/Queen.h"
-#include "pieces/Knight.h"
-#include "pieces/Rook.h"
-#include "pieces/Bishop.h"
-
 /* Board
  * Contains all of the game-wide variables and code
  * also handles player turns and pieces, and the visual
@@ -24,31 +17,21 @@ class Board
 	
 	Board();
 	~Board();
+	
 	bool whiteTurn;
 	
-	void userInput(std::string&);
 	void setStartingBoard(bool);
-	void drawBoard();
-	void toggleSize(); // large/small
-	
     void movePiece(Pos&, Pos&); // move from a to b if valid on this piece
     
 	Pos sqSize; // size of a singe square on board
 	Pos offset; // offset of where the center of that square is
 	
-	private:
-	
 	std::vector<Piece*> whitePieces;
 	std::vector<Piece*> blackPieces;
-	
-	// line characters for drawing board
-	cchar_t li;
-	cchar_t ld;
 	
 	Piece* gameBoard[MAX_ROW_COL][MAX_ROW_COL]; // remember to free later
 	
 	void cleanBoard();
-	bool largeBoard = false;
 };
 
 #endif
